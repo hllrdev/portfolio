7ce2a94d4fcc5ef740a15d938888eb31f6ac5f9b
Héuller <hllrdev@gmail.com>
2023-08-21 18:36:02 -0300

    added output

--- components/Curriculum/index.vue
+++ components/Curriculum/index.vue
@@ -2,9 +2,9 @@
     <div id="curriculum" class="py-10">
         <h2 class="text-center font-semibold pb-10">Baixe o meu currículo aqui</h2>
         <div class="flex justify-center">
-            <button class="btn">
+            <NuxtLink target="_blank" to="https://drive.google.com/file/d/1jKfellyOTW861d8j2u4w0O43pFTE1PvN/view?usp=sharing" class="btn">
                 <Icon class="text-2xl" name="ic:baseline-attach-file"/>download
-            </button>
+            </NuxtLink>
         </div>       
     </div>
 </template>
