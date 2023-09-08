6e9f87807eb9b4ead2ced1fddda247005aa46652
Héuller Soares Vilela Silva <hllrdev@gmail.com>
2023-09-08 17:03:09 -0300

    Update index.vue

--- components/Curriculum/index.vue
+++ components/Curriculum/index.vue
@@ -2,7 +2,7 @@
     <div id="curriculum" class="py-10">
         <h2 class="text-center font-semibold pb-10">Baixe o meu currículo aqui</h2>
         <div class="flex justify-center">
-            <NuxtLink target="_blank" to="https://drive.google.com/file/d/1jKfellyOTW861d8j2u4w0O43pFTE1PvN/view?usp=sharing" class="btn">
+            <NuxtLink target="_blank" to="https://drive.google.com/file/d/1Ay7pRrZKvoTIemOrCKScGON4G1QSrgC7/view?usp=sharing" class="btn">
                 <Icon class="text-2xl" name="ic:baseline-attach-file"/>download
             </NuxtLink>
         </div>       
